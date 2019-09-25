b242b51cf14693c4621a9ee70886288ac102306d
Dan Kreiger <dan@dankreiger.com>
2019-09-25 22:24:53 +0200

    chore(*): more stuff

--- src/components/App/App.routes.tsx
+++ src/components/App/App.routes.tsx
@@ -15,17 +15,17 @@ const AppRoutes = (): any => {
   const transitions = useTransition(location, (location: any) => location.key, {
     from: {
       opacity: 0,
-      width: "100%",
-      transform: "translate3d(100%, 0, 0) scale(0)"
+      width: "100%"
+      // transform: "translate3d(100%, 0, 0) scale(0)"
     },
     enter: {
       opacity: 1,
-      transform: "translate3d(0,0,0) scale(1)",
+      // transform: "translate3d(0,0,0) scale(1)",
       backgroundColor: getBgColor((location as any).pathname)
     },
     leave: {
-      opacity: 0,
-      transform: "translate3d(-50%,0,0) scale(0)"
+      opacity: 0
+      // transform: "translate3d(-50%,0,0) scale(0)"
     },
     config: {
       duration: ROUTER_ANIMATION_DURATION,
